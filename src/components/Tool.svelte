cf76a4757164612df0655f76e9afe2b2de73136c
Ned Zimmerman <ned@bight.dev>
2020-04-23 09:59:42 -0600

    feat: tweak card layout

--- src/components/Tool.svelte
+++ src/components/Tool.svelte
@@ -11,19 +11,19 @@
     </header>
     <aside class="meta">
         {#if tool.nicheNames }
-        <div class="card__meta"><svg class="icon icon--toolkit" viewBox="0 0 20 20" aria-hidden="true" focusable="false"><use href="/images/toolkit.svg#toolkit" /></svg> Tool Type: { fancyConcat(tool.nicheNames) }</div>
-        {/if}
-        {#if tool.license }
-        <div class="card__meta"><svg class="icon icon--settings" viewBox="0 0 20 20" aria-hidden="true" focusable="false"><use href="/images/settings.svg#settings" /></svg> License: { tool.license }</div>
+        <div class="card__meta"><span class="screen-reader-text">tool type: </span><strong>{ fancyConcat(tool.nicheNames) }</strong></div>
         {/if}
         {#if tool.pricing }
         <div class="card__meta"><svg class="icon icon--report" viewBox="0 0 20 20" aria-hidden="true" focusable="false"><use href="/images/report.svg#report" /></svg> Pricing: { tool.pricing }</div>
         {/if}
+		{#if tool.license }
+        <div class="card__meta"><svg class="icon icon--settings" viewBox="0 0 20 20" aria-hidden="true" focusable="false"><use href="/images/settings.svg#settings" /></svg> Licensing: { tool.license }</div>
+        {/if}
         {#if tool.sectors && tool.sectors.length > 0 }
-        <div class="card__meta"><svg class="icon icon--sector" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><use href="/images/sector.svg#sector" /></svg> Sectors: { fancyConcat(tool.sectors) }</div>
+        <div class="card__meta"><svg class="icon icon--sector" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><use href="/images/sector.svg#sector" /></svg> Sector: { fancyConcat(tool.sectors) }</div>
         {/if}
         {#if tool.languages && tool.languages.length > 0 }
-        <div class="card__meta"><svg class="icon icon--info" viewBox="0 0 20 20" aria-hidden="true" focusable="false"><use href="/images/info.svg#info" /></svg> Languages: { fancyConcat(tool.languages) }</div>
+        <div class="card__meta"><svg class="icon icon--info" viewBox="0 0 20 20" aria-hidden="true" focusable="false"><use href="/images/info.svg#info" /></svg> Language: { fancyConcat(tool.languages) }</div>
         {/if}
     </aside>
 </article>
