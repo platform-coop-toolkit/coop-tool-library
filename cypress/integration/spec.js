2c7ffea7d2b14dc797e47670c0d19d2734565897
Ned Zimmerman <ned@bight.dev>
2020-06-15 16:05:31 -0600

    fix: resolve Cypress issue

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -6,14 +6,4 @@ describe('Sapper template app', () => {
 	it('has the correct <h1>', () => {
 		cy.contains('h1', 'Cooperative Tool Library')
 	});
-
-	it('navigates to /about', () => {
-		cy.get('nav a').contains('about').click();
-		cy.url().should('include', '/about');
-	});
-
-	it('navigates to /glossary', () => {
-		cy.get('nav a').contains('glossary').click();
-		cy.url().should('include', '/glossary');
-	});
 });
