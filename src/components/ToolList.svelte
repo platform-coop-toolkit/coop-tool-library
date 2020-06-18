90cf930eae20b6565371c6a77a18d2c9a6830177
Alan Harnum <waharnum@gmail.com>
2020-06-18 10:31:47 -0400

    Refactoring towards more generic filter functions in ToolList.svelte

--- src/components/ToolList.svelte
+++ src/components/ToolList.svelte
@@ -5,21 +5,24 @@
 
     import Card from './Card.svelte';
 
-    function hasNiche(tool, nicheFilter) {
+    function hasNiche(tool, valueToCheck, nicheFilter) {
         if(nicheFilter === 'All') return true;
         else return Object.keys(tool.niches).includes(nicheFilter);        
     }
 
-    function hasPricing(tool, pricingFilter) {
-        if(pricingFilter === 'All') return true;
-        else return tool.pricing === pricingFilter;
+    function meetsFilterCriteria(tool, valueToCheck, filter, checkFunction) {
+        if(checkFunction) {
+            return checkFunction(tool, valueToCheck, filter);
+        }
+        if(filter === 'All') return true;
+        else return tool[valueToCheck] === filter;
     }
-    
+        
 </script>
 
 <div class="cards" >			
     {#each tools as tool}        
-        {#if hasNiche(tool, nicheFilter) && hasPricing(tool, pricingFilter)}							
+        {#if hasNiche(tool, "niches", nicheFilter, hasNiche) && meetsFilterCriteria(tool, "pricing", pricingFilter)}							
             <Card tool={tool} />				
         {/if}
     {/each} 
