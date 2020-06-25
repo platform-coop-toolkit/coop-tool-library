6381d335c965f4f309f1ed6fcbf74202eed0eabc
Alan Harnum <waharnum@gmail.com>
2020-06-25 13:16:07 -0400

    Filter expansion / collapse per design.

--- src/components/CheckboxGroup.svelte
+++ src/components/CheckboxGroup.svelte
@@ -25,12 +25,21 @@
     function handleDeSelectAllClick() {                             
             checkedOptions = [];        
     }
+    
+    export let isExpanded = false;
+
+    function handleExpandClick() {
+        isExpanded = !isExpanded;
+    }
 
 </script>
 
 <div class="accordion accordion--filter-list">
-    <div class="accordion__pane">
-        <p class="accordion__heading">{filterTitle}</p>
+    <div class="accordion__pane{isExpanded ? "--expanded" : ""}">
+    <button class="accordion__control" aria-expanded={isExpanded} type="button" on:click={handleExpandClick}>
+			{filterTitle}
+			<svg class="icon icon--chevron-down" viewBox="0 0 20 20" aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg"><path id="chevron-down" d="m10 15a1 1 0 0 1 -.71-.29l-5-5a1 1 0 0 1 1.42-1.42l4.29 4.3 4.29-4.3a1 1 0 0 1 1.42 1.42l-5 5a1 1 0 0 1 -.71.29z" fill="currentColor"></path></svg>
+		</button>        
         <div class="accordion__content">
        
         
