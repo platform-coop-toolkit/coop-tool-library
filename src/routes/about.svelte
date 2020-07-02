c4b918468dbdf7d07f084daab891571b6ace712e
Alan Harnum <waharnum@gmail.com>
2020-07-02 10:43:32 -0400

    Full-width grid container utility class; about page content.

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -8,4 +8,20 @@
 	</div>
 </div>
 
-<p>TODO.</p>
+<div class="grid-container--full-width">
+
+	<h2>What is the Tool Library?</h2>
+	<p>The Cooperative Tool Library is a website where people can find software tools for their co-ops, and share the tools they use with other co-ops.</p>
+
+	<h2>Who is the Tool Library for?</h2>
+	<p>The Tool Library was built to be centred around cooperatives, prospective cooperatives, and those who assist in the development of cooperatives. However, anyone in the cooperative ecosystem can use this library.</p>
+
+	<h2>Who runs the Tool Library?</h2>
+	<p>This website is help run by everyone in the co-op community who can contribute by adding tools, editing their information to ensure accuracy, and those who write reviews. A small amount of moderation is done by the Platform Cooperative Consortium when tools are added. </p>
+
+	<h2>Who created the Tool Library?</h2>
+	<p>The Cooperative Tool Library is created as a collaboration between the Inclusive Design Research Centre, our many co-designers from the cooperative community, and the Platform Cooperative Consortium.</p>
+
+	<h2>Contact us</h2>
+	<p>If you have any feedback this website, or have any questions about how to use it, please contact the Platform Cooperative Consortium at info@platform.coop.</p>
+</div>
