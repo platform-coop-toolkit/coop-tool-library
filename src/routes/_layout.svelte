b12aaa0c1fcaa640d7545a7d9771ae9f03826afd
Ned Zimmerman <ned@bight.dev>
2020-07-14 09:59:34 -0600

    fix: update link to share your tools

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -2,7 +2,7 @@
 	import GlobalNav from '../components/GlobalNav.svelte';
 	import Nav from '../components/Nav.svelte';
 
-	export let segment;	
+	export let segment;
 </script>
 
 <style lang="scss" global>
@@ -12,13 +12,13 @@
 <header role="banner">
 	<div class="container">
 		<GlobalNav />
-		<a href="/" class="link link--brand"><svg viewBox="0 0 32 32" class="icon icon--pcc" aria-hidden="true" focusable="true"><use href="/images/tools.svg#tools"></use></svg> <span class="brand__title screen-reader-text">Cooperative Tool Library</span></a>
+		<a href="/" class="link link--brand"><svg viewBox="0 0 32 32" class="icon icon--tools" aria-hidden="true" focusable="true"><use href="/images/tools.svg#tools"></use></svg> <span class="brand__title screen-reader-text">Cooperative Tool Library</span></a>
 		<div class="inner">
 			<Nav {segment}/>
 		</div>
 	</div>
 </header>
-	
+
 <div class="wrap container search-results" role="document">
 	<div class="content">
 		<main>
